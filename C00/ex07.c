0f265d169e16d3e14271647c7aa2a6ad1ba4d5e5
Mher Saribekyan <saribekyanmher@gmail.com>
2025-11-20 23:05:02 +0400

    fixed ex07

--- C00/ex07.c
+++ C00/ex07.c
@@ -8,9 +8,11 @@ int	main()
 
 	c = '\n';
 	ft_putnbr(1010);	
+ 	write(1, &c, 1);
+ 	ft_putnbr(0);
+ 	write(1, &c, 1);
+ 	ft_putnbr(-1010);
 	write(1, &c, 1);
-	ft_putnbr(0);
-	write(1, &c, 1);
-	ft_putnbr(-1010);
+	ft_putnbr(-2147483648);
 	return 0;
 }
