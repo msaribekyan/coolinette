c68303c091228b47ee061865d7ab444cedcdcdae
Mher Saribekyan <saribekyanmher@gmail.com>
2025-11-28 21:46:46 +0400

    news

--- tests/C02/ex04.c
+++ tests/C02/ex04.c
@@ -1,11 +1,18 @@
+int	ft_str_is_lowercase(char *str);
+
 #include <stdio.h>
 
-void ft_ultimate_div_mod(int *a, int *b);
+int main()
+{
+	char str1[20] = "helloworld";
+	char str2[20] = "hello world";
+	char str3[20] = "hello\tworld";
+	char str4[20] = "Hjo20Avjka3jiiAS";
+	char str5[20] = "0";
 
-int main(){
-	int a = 1010;
-	int b = 7;
-	ft_ultimate_div_mod(&a, &b);
-	printf("%d %d", a, b);
-	return 0;
+	printf("%d\n", ft_str_is_lowercase(str1));
+	printf("%d\n", ft_str_is_lowercase(str2));
+	printf("%d\n", ft_str_is_lowercase(str3));
+	printf("%d\n", ft_str_is_lowercase(str4));
+	printf("%d\n", ft_str_is_lowercase(str5));
 }
